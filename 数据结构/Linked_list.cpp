c18dd58d8b53340801cbb89503a0a34eaa297b51
corazon <chopperfan@qq.com>
2019-03-11 13:45:43 +0800

    添加项目文件。

---
+++ 数据结构/Linked_list.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct LNode {
+	int data;
+	LNode *next;
+};
+
+LNode* create_bottom(int n) {
+	LNode * head;
+	LNode * head_next;
+	head = (LNode*)malloc(sizeof(LNode));
+	head->next = NULL;
+	head_next = NULL;
+	int data;
+	cout << "please input " << n << " codes " << endl;
+	int i;
+	for (i = 0; i < n; i++) {
+		cin >> data;
+		LNode *new_node;
+		new_node = (LNode*)malloc(sizeof(LNode));
+		new_node->data = data;
+		new_node->next = head_next;
+		head_next = new_node;
+		head->next = head_next;
+	}
+	return head;
+};
+
+LNode* create_front(int n) {
+	LNode * head;
+	LNode * head_next;
+	head = (LNode*)malloc(sizeof(LNode));
+	head->next = NULL;
+	head_next = head;
+	int data;
+	cout << "please input " << n << " codes " << endl;
+	int i;
+	for (i = 0; i < n; i++) {
+		cin >> data;
+		LNode *new_node;
+		new_node = (LNode*)malloc(sizeof(LNode));
+		new_node->data = data;
+		new_node->next = NULL;
+		head_next->next = new_node;
+		head_next = head_next->next;
+	}
+	return head;
+}
+
+void printLinked_List(LNode *head) {
+	LNode *p;
+
+	p = head->next;
+	
+	if (head->next == NULL) {
+		cout << "Empty Linked List!" << endl;
+		exit(0);
+	}
+	do {
+		cout << p->data;
+		p = p->next;
+	} while (p != NULL);
+}
+
+int main()
+{
+	LNode * head;
+	head = create_front(5);
+	printLinked_List(head);
+	return 0;
+}
